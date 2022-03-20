8894a153bfca4efa51cce10800ebba59e3560489
Dave Gauer <dave@ratfactor.com>
2022-03-20 10:07:26 -0400

    Provide lib path as first param, skip zig-cache

--- build.sh
+++ build.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
 
-lib_dir=/tmp/zig/lib/std/
+# First param of script must be path to zig std lib
+# like so:
+#
+#   build.sh /home/dave/zig/lib/std/
+#
+# Second param CAN be wildcard match:
+#
+#   $ ./build.sh /home/dave/zig/lib/std/ queue
+#   atomic/queue.zig
+#   priority_queue.zig
+#   priority_dequeue.zig
+#
+lib_dir=$1
 for file in $(find $lib_dir -name '*.zig')
 do
-    # Only output files matching argument 1
-    # example:
-    #   $ ./build.sh queue
-    #   atomic/queue.zig
-    #   priority_queue.zig
-    #   priority_dequeue.zig
-    if [[ $1 && $file != *$1* ]]
-    then
-        continue
-    fi
+    # Only output files matching argument 2
+    [[ $1 && $file != *$1* ]] && continue
+
+    # Skip zig-cache!
+    [[ $file == *zig-cache* ]] && continue
 
     relative_file=${file#"$lib_dir"}
     output_file="output/$relative_file.html"
